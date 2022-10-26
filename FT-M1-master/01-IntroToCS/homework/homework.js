71ba5de69d976c84928f3627b9f28263ae80dc22
kalethabh@gmailcom <kalethabh@gmail.com>
2022-10-25 21:03:15 -0500

    M1 Class 1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,13 +1,17 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  // tu codigo aca:
+  return parseInt(num,2);
 }
-
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  // tu codigo aca:
+  var xd = [];
+  while(num > 0){
+    xd.push(num%2);
+    num = Math.floor(num/2);
+  }
+  return xd.reverse().join("");
 }
 
 
